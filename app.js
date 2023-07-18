84d1ac4f4375c6522185a5541db191b40115cdb5
LeonardoGarcia-Estralagio <estralagio@gmail.com>
2023-07-18 03:10:13 -0300

    feat: register.html y ruta en app.js

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ app.get("/", (req, res) =>
   res.sendFile(path.join(__dirname, "views", "home.html"))
 );
 
+app.get("/register", (req, res) =>
+  res.sendFile(path.join(__dirname, "views", "register.html"))
+);
+
 app.listen(PORT, () =>
   console.log(`server running in http://localhost:${PORT}`)
 );
