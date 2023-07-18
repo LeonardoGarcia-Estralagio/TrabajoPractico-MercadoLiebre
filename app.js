16c5fb8d21ebb197a914b7bbc9e69d6daff0f110
LeonardoGarcia-Estralagio <estralagio@gmail.com>
2023-07-18 18:45:31 -0300

    feat: formulario de registro y login html

--- app.js
+++ app.js
@@ -13,6 +13,10 @@ app.get("/register", (req, res) =>
   res.sendFile(path.join(__dirname, "views", "register.html"))
 );
 
+app.get("/login", (req, res) =>
+  res.sendFile(path.join(__dirname, "views", "login.html"))
+);
+
 app.listen(PORT, () =>
   console.log(`server running in http://localhost:${PORT}`)
 );
